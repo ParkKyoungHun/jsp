9240b231d20b44373bd9fd560d4358bb7dab3d16
Administrator <Administrator@192.168.100.164>
2017-09-11 20:12:40 +0900

    t

--- jsp/src/servlet/GoodsServlet.java
+++ jsp/src/servlet/GoodsServlet.java
@@ -2,7 +2,9 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -26,12 +28,27 @@ public class GoodsServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 			request.setCharacterEncoding("utf-8");
+			String command = request.getParameter("command");
+			if(command.equals("list")) {
+				List<GoodsInfo> list = gs.selectGoodsList(null);
+				List<VendorInfo> vList = gs.selectVendorList(null);
+				request.setAttribute("goodsList", list);
+				request.setAttribute("vendorList", vList);
+				String url = "/goods/goods_list.jsp";
+				RequestDispatcher rd = request.getRequestDispatcher(url);
+				rd.forward(request, response);
+			}
 	} 
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		String command = request.getParameter("command");
+		if(command==null) {
+			String param = request.getParameter("param");
+			Map<String, String> hm = g.fromJson(param, HashMap.class);
+			command = hm.get("command");
+		} 
 		if(command.equals("list")) {
 			List<GoodsInfo> list = gs.selectGoodsList(null);
 			List<VendorInfo> vList = gs.selectVendorList(null);
@@ -40,6 +57,10 @@ public class GoodsServlet extends HttpServlet {
 			String url = "/goods/goods_list.jsp";
 			RequestDispatcher rd = request.getRequestDispatcher(url);
 			rd.forward(request, response);
+		}else if(command.equals("vendorcombo")) {
+			List<VendorInfo> vList = gs.selectVendorList(null);
+			String result = g.toJson(vList);
+			doProcess(response, result);
 		}
 	}
 	
